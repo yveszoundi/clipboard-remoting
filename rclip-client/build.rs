88a9d91c0e8056e08aaa59196d8ee363c504fa77
Yves Zoundi <yveszoundi@gmail.com>
2022-04-02 22:12:51 -0400

    Avoid external clipboard program dependencies

--- rclip-client/build.rs
+++ rclip-client/build.rs
@@ -4,12 +4,16 @@ fn main() {
       println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
       println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
    } else {
+       // Deal with BSD platforms linker issues (XCB and similar libraries)
        if cfg!(target_os="openbsd") {
            println!("cargo:rustc-link-search=/usr/X11R6/lib");
        } else if cfg!(target_os="freebsd") {
+           println!("cargo:rustc-arg=-Wl");
            println!("cargo:rustc-link-search=/usr/local/lib");
        } else if cfg!(target_os="netbsd") {
-           println!("cargo:rustc-link-search=/usr/X11R7/lib");
+           println!("cargo:rustc-arg=-Wl");
+           println!("cargo:rustc-link-search=/usr/pkg/lib");
+           println!("cargo:rustc-link-arg=-rpath=/usr/pkg/lib");
        }
    }
 }
