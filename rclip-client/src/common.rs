1ae80ee0b2c6697418a23950ea19b11c09773c08
Yves Zoundi <yveszoundi@gmail.com>
2022-04-07 07:12:00 -0400

    Initial SSL support
    - Add SSL support with rustls
      - Update client and server code for SSL
      - Update documentation accordingly
    - Add new server command to clear the clipboard
    - Use consistent naming conventions for flags
    - Use cargo manifest data for the application metadata

--- rclip-client/src/common.rs
+++ rclip-client/src/common.rs
@@ -1,7 +1,18 @@
-use std::error::Error;
-use std::net::TcpStream;
-use std::io::{Read, Write, BufReader};
+use std::convert::TryFrom;
+use std::io;
+use std::time::SystemTime;
 
+use rustls::client::ServerCertVerified;
+use rustls::ServerName;
+use std::error::Error;
+use std::fs;
+use std::sync::Arc;
+use tokio::io::{split, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+use tokio_rustls::rustls::Certificate;
+use tokio_rustls::TlsConnector;
+
+const BUFFER_CAP: usize = 512;
 pub const DEFAULT_SERVER_HOST_STR: &str = "127.0.0.1";
 pub const DEFAULT_SERVER_PORT_STR: &str = "10080";
 
@@ -21,6 +32,30 @@ impl std::fmt::Display for ClipboardCmd {
                 write!(f, "WRITE:")
             }
         }
+    }
+}
+
+struct AcceptSpecificCertsVerifier {
+    certs: Vec<rustls::Certificate>,
+}
+
+impl rustls::client::ServerCertVerifier for AcceptSpecificCertsVerifier {
+    fn verify_server_cert(
+        &self,
+        end_entity: &Certificate,
+        _intermediates: &[Certificate],
+        _server_name: &ServerName,
+        _scts: &mut dyn Iterator<Item = &[u8]>,
+        _ocsp_response: &[u8],
+        _now: SystemTime,
+    ) -> Result<ServerCertVerified, rustls::Error> {
+        let presented_cert = end_entity; //&intermediates[0];
+        for cert in &self.certs {
+            if presented_cert == cert {
+                return Ok(rustls::client::ServerCertVerified::assertion());
+            }
+        }
+        return Err(rustls::Error::General("Unknown issuer".to_string()));
     }
 }
 
@@ -50,32 +85,70 @@ pub fn set_clipboard_contents(clipboard_text: String) -> Result<(), Box<dyn Erro
     Ok(())
 }
 
-pub fn send_cmd(server_host: &str, port_number: u16, clipboard_cmd: ClipboardCmd) -> Result<(), Box<dyn Error + Send + Sync>> {
+pub async fn send_cmd(
+    server_host: &str,
+    port_number: u16,
+    key_pub_loc: &str,
+    clipboard_cmd: ClipboardCmd,    
+) -> Result<(), Box<dyn Error + Send + Sync>> {
     let input = format!("{}", clipboard_cmd);
 
-    match TcpStream::connect(format!("{}:{}", server_host, port_number)) {
-        Ok(mut stream) => {
-            let request = input.as_bytes();
-            stream.write(request)?;
+    let key_pub_bytes =
+        fs::read(key_pub_loc)?;
 
-            let mut reader = BufReader::new(&stream);
-            let mut buf = String::new();
-            reader.read_to_string(&mut buf).unwrap();
+    let config = rustls::ClientConfig::builder()
+        .with_safe_defaults()
+        .with_custom_certificate_verifier(Arc::new(AcceptSpecificCertsVerifier {
+            certs: vec![Certificate(key_pub_bytes)],
+        }))
+        .with_no_client_auth();
 
-            let response = buf.clone();
+    let connector = TlsConnector::from(Arc::new(config));
+    let addr = format!("{}:{}", server_host, port_number);
+    let stream = TcpStream::connect(addr.clone()).await?;
 
-            if response.starts_with("SUCCESS:") {
-                if input.starts_with("READ:") {
-                    let clipboard_text = response.chars().skip("SUCCESS:".len()).collect();
-                    set_clipboard_contents(clipboard_text)?;
+    // Just need to resolve a domain, as IP addresses are not supported to use the actual server IP
+    // see also https://docs.rs/rustls/latest/rustls/enum.ServerName.html
+    let domain = rustls::ServerName::try_from("localhost")
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "Invalid dnsname"))?;
+
+    let stream = connector.connect(domain, stream).await?;
+
+    let (mut reader, mut writer) = split(stream);
+
+    writer.write_all(input.as_bytes()).await?;
+
+    let mut response = String::new();
+
+    loop {
+        let mut buf_vec = vec![0; BUFFER_CAP];
+        let bytes_read = match reader.read(&mut buf_vec).await {
+            Ok(n) => match String::from_utf8(buf_vec[0..n].to_vec()) {
+                Ok(data) => {
+                    response.push_str(&data);
+                    n
                 }
-            } else {
-                return Err(response.into());
+                Err(e) => {
+                    return Err(format!("Cannot read server data; err = {}", e.to_string()).into());
+                }
+            },
+            Err(e) => {
+                return Err(format!("Failed to read from socket; err = {}", e.to_string()).into());
             }
-        },
-        Err(ex) => {
-            return Err(format!("Could not connect to clipboard server at '{}:{}'! {}", server_host, port_number, ex.to_string()).into());
+        };
+
+        if bytes_read == 0 || bytes_read < BUFFER_CAP {
+            break;
         }
+    }
+
+    if response.starts_with("SUCCESS:") {
+        if input.starts_with("READ:") || input.starts_with("CLEAR:") {
+            let clipboard_text = response.chars().skip("SUCCESS:".len()).collect();
+            set_clipboard_contents(clipboard_text)?;
+        }
+    } else {
+        return Err(response.into());
     }
 
     Ok(())
--- rclip-server/src/main.rs
+++ rclip-server/src/main.rs
@@ -1,14 +1,23 @@
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use clap::{App, Arg};
+use std::error::Error;
+use std::fs;
+use std::io;
+use tokio::io::{split, AsyncReadExt, AsyncWriteExt};
+
 use std::sync::{Arc, Mutex};
 
-const CMD_READ   : &str  = "READ:";
-const BUFFER_CAP : usize = 512;
-const CMD_WRITE  : &str  = "WRITE:";
+use tokio::net::TcpListener;
+use tokio_rustls::rustls::{self, Certificate, PrivateKey};
+
+use tokio_rustls::TlsAcceptor;
+
+const CMD_READ: &str = "READ:";
+const CMD_WRITE: &str = "WRITE:";
+const CMD_CLEAR: &str = "CLEAR:";
+const BUFFER_CAP: usize = 512;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let app = App::new(option_env!("CARGO_PKG_NAME").unwrap_or("Unknown"))
         .version(option_env!("CARGO_PKG_VERSION").unwrap_or("Unknown"))
         .author("Yves Zoundi")
@@ -19,70 +28,127 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .help("IP address to bind to")
                 .required(false)
                 .default_value("127.0.0.1")
-                .takes_value(true)
-        ).arg(
+                .takes_value(true),
+        )
+        .arg(
             Arg::with_name("port")
                 .long("port")
                 .help("Port number")
                 .required(false)
                 .default_value("10080")
-                .takes_value(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("der-cert-priv")
+                .long("der-cert-priv")
+                .help("Private DER certificate key")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("der-cert-pub")
+                .long("der-cert-pub")
+                .help("Public DER certificate key")
+                .required(true)
+                .takes_value(true),
         );
 
-    let run_matches= app.to_owned().get_matches();
+    let run_matches = app.to_owned().get_matches();
 
     let host = match run_matches.value_of("host") {
         Some(proposed_hostname) => proposed_hostname,
-        None => "127.0.0.1"
+        None => "127.0.0.1",
     };
 
     let port = match run_matches.value_of("port") {
         Some(proposed_port) => proposed_port,
-        None => "10080"
+        None => "10080",
     };
 
     let con_string = format!("{}:{}", host, port);
+
+    let key_pub_bytes = match run_matches.value_of("der-cert-pub") {
+        Some(der_cert_pub) => fs::read(der_cert_pub)?,
+        None => return Err("Cannot find cert".into())
+    };
+
+    let key_priv_bytes = match run_matches.value_of("der-cert-priv") {
+        Some(der_cert_priv) => fs::read(der_cert_priv)?,
+        None => return Err("Cannot find cert".into())
+    };
+
+    let config = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(vec![Certificate(key_pub_bytes)], PrivateKey(key_priv_bytes))
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))?;
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
     let listener = TcpListener::bind(con_string).await?;
     let clipboard = Arc::new(Mutex::new("".to_string()));
 
-    println!("Starting '{}' on {} with port {}.", app.get_name(), host, port);
+    println!(
+        "Starting '{}' on {} with port {}.",
+        app.get_name(),
+        host,
+        port
+    );
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let (stream, _) = listener.accept().await?;
+        let acceptor = acceptor.clone();
         let clipboard_copy = clipboard.clone();
 
         tokio::spawn(async move {
             let mut request = String::new();
-            let (mut reader, mut writer) = socket.split();
 
-            loop {
-                let mut buf_vec = vec![0; BUFFER_CAP];
-                let bytes_read = match reader.read(&mut buf_vec).await {
-                    Ok(n) => {
-                        match String::from_utf8(buf_vec[0..n].to_vec()) {
-                            Ok(data) => {
-                                request.push_str(&data);
-                                n
+            match acceptor.accept(stream).await {
+                Ok(stream) => {
+                    let (mut reader, mut writer) = split(stream);
+
+                    loop {
+                        let mut buf_vec = vec![0; BUFFER_CAP];
+                        let bytes_read = match reader.read(&mut buf_vec).await {
+                            Ok(n) => match String::from_utf8(buf_vec[0..n].to_vec()) {
+                                Ok(data) => {
+                                    request.push_str(&data);
+                                    n
+                                }
+                                Err(e) => {
+                                    return Err(format!(
+                                    "Failed to decode request; err = {}",
+                                    e.to_string()
+                                )
+                                           .into());
+                                }
                             },
                             Err(e) => {
-                                return Err(format!("Failed to decode response; err = {}", e.to_string()).into());
+                                return Err(format!(
+                                    "Failed to read from socket; err = {}",
+                                    e.to_string()
+                                )
+                                           .into());
                             }
+                        };
+
+                        if bytes_read == 0 || bytes_read < BUFFER_CAP {
+                            break;
                         }
-                    },
-                    Err(e) => {
-                        return Err(format!("Failed to read from socket; err = {}", e.to_string()).into());
                     }
-                };
 
-                if bytes_read == 0 || bytes_read < BUFFER_CAP {
-                    break;
+                    let response = handle_message(request, clipboard_copy);
+
+                    if let Err(e) = writer.write_all(response.as_bytes()).await {
+                        return Err(e.to_string());
+                    }
                 }
-            }
-
-            let response = handle_message(request, clipboard_copy);
-
-            if let Err(e) = writer.write_all(response.as_bytes()).await {
-                return Err(e.to_string());
+                Err(e) => {
+                     return Err(format!(
+                                    "Error with TLS negotiation; err = {}",
+                                    e.to_string()
+                                )
+                                           .into());
+                }
             }
 
             Ok(())
@@ -100,11 +166,16 @@ fn handle_message(data: String, clipboard: Arc<Mutex<String>>) -> String {
                 old_clipboard.clear();
                 old_clipboard.push_str(new_clipboard);
                 return format!("SUCCESS:{}", new_clipboard);
+            } else if data.starts_with(CMD_CLEAR) {
+                let new_clipboard = "";
+                old_clipboard.clear();
+                old_clipboard.push_str(new_clipboard);
+                return format!("SUCCESS:{}", new_clipboard);
             } else {
                 return format!("ERROR:Unknown message {}.", data);
             }
-        },
-        Err(ex) =>{
+        }
+        Err(ex) => {
             return format!("ERROR:Could not acquire clipboard data. {}", ex);
         }
     }
